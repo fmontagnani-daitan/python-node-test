99edde5b60eba80206b7906e42c04c7164d44f70
fmontagnani-daitan <fmontagnani@daitan.com>
2021-12-29 13:47:11 -0300

    refactor: Refactor logic and added typing on files

--- pythoncalctest/src/calculator/calculator.py
+++ pythoncalctest/src/calculator/calculator.py
@@ -1,4 +1,5 @@
-from src.operations.operations import soma, subtract, multiply, divide # pylint: disable=import-error
+from typing import Union
+from src.operations.operations import soma, subtract, multiply, divide
 
 class Calculator():
     def __init__(self) -> None:
@@ -6,21 +7,26 @@ class Calculator():
         self._subtract = subtract
         self._multiply = multiply
         self._divide = divide
-    
-    def validate_numbers(self, num1, num2) -> bool:
+        self._operations = [0, 1, 2, 3]
+
+    def validate_options(self, operation_code: int, num1: int, num2: int) -> bool:
+        if operation_code not in self._operations:
+            raise ValueError('Operation code must be between 0 and 3')
+
         if not isinstance(num1, int) or not isinstance(num2, int):
             raise TypeError('Error: Operands must be numbers')
+
         return True
-    
-    def execute_operation(self, operation_code, num1, num2):
-        if self.validate_numbers(num1, num2):
+
+    def execute_operation(self, operation_code: int, num1: int, num2: int) -> Union[int, float]:    # pylint: disable=inconsistent-return-statements
+        is_valid_options = self.validate_options(operation_code, num1, num2)
+
+        if is_valid_options:
             if operation_code == 0:
                 return self._sum(num1, num2)
-            elif operation_code == 1:
+            if operation_code == 1:
                 return self._subtract(num1, num2)
-            elif operation_code == 2:
+            if operation_code == 2:
                 return self._multiply(num1, num2)
-            elif operation_code == 3:
+            if operation_code == 3:
                 return self._divide(num1, num2)
-            else:
-                raise ValueError(message='Operation code must be between 0 and 3')
--- pythoncalctest/src/operations/operations.py
+++ pythoncalctest/src/operations/operations.py
@@ -1,11 +1,11 @@
-def soma(num1, num2):
+def soma(num1: int, num2: int) -> int:
     return num1 + num2
 
-def subtract(num1, num2):
+def subtract(num1: int, num2: int) -> int:
     return num1 - num2
 
-def multiply(num1, num2):
+def multiply(num1: int, num2: int) -> int:
     return num1 * num2
 
-def divide(num1, num2):
+def divide(num1: int, num2: int) -> float:
     return num1 / num2
