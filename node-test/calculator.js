88fd76374679edeabf5df5a436382698ef99d78d
fmontagnani-daitan <fmontagnani@daitan.com>
2022-02-09 11:27:44 -0300

    fix: fixed js folder

--- node-test/calculator.js
+++ node-test/calculator.js
@@ -1,21 +1,28 @@
 import { add, subtract, multiply, divide } from './operations.js';
 
-const _validateNumbers = (num1, num2) => {
+const OPERATIONS = {
+    0: add,
+    1: subtract,
+    2: multiply,
+    3: divide,
+};
+
+const _validateOptions = (operationId, num1, num2) => {
+    if (!(operationId in OPERATIONS)) {
+        throw new Error('Operation code must be between 0 and 3')
+    }
+
     if (isNaN(num1) || isNaN(num2)) {
         throw new Error('Operands must be numbers')
     }
-};
-
-const _executeOperation = (operation, num1, num2) => {
-    _validateNumbers(num1, num2);
-    return operation(num1, num2);
+    return OPERATIONS[operationId];
 };
 
 const calculator = {
-    0: (num1, num2) => _executeOperation(add, num1, num2),
-    1: (num1, num2) => _executeOperation(subtract, num1, num2),
-    2: (num1, num2) => _executeOperation(multiply, num1, num2),
-    3: (num1, num2) => _executeOperation(divide, num1, num2),
+    execute: (operationId, num1, num2) => {
+        const operation = _validateOptions(operationId, num1, num2);
+        return operation(num1, num2);
+    },
 };
 
 export default calculator;
--- node-test/test/main.test.js
+++ node-test/test/main.test.js
@@ -5,18 +5,18 @@ process.argv = ['node', 'jest', '0', '1', '3']
 import { main } from '../main.js';
 
 jest.mock('../calculator.js', () => ({
-    0: jest.fn(),
+    execute: jest.fn(),
 }));
 
 describe('[MAIN]', () => {
     it('calls correctly operation', () => {
         main();
 
-        expect(calculator[0]).toHaveBeenCalledWith(1, 3);
+        expect(calculator.execute).toHaveBeenCalledWith(0, 1, 3);
     });
 
     it('correctly handles exception', () => {
-        calculator[0].mockReturnValue(new Error('foo'));
+        calculator.execute.mockReturnValue(new Error('foo'));
         try {
             main();
         } catch(error) {
